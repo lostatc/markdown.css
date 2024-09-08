0dadcc994fda6f5ae853fe7e4f6b1b8379d98313
Wren Powell <wrenp@duck.com>
2024-09-08 13:31:42 -0400

    Add script for rendering a test document

---
+++ test/render.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+declare -r test_dir="$(dirname "${BASH_SOURCE}")"
+
+pandoc --pdf-engine weasyprint --css "${1}" --standalone --output "${test_dir}/output.pdf" "${test_dir}/input.md"
